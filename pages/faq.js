413c8f04e5ae4adb0de458ef14ed91787b634c06
Chad Hamre <chamre@medium.com>
2020-07-04 16:32:34 -0700

    add list to faq

--- pages/faq.js
+++ pages/faq.js
@@ -2,6 +2,7 @@ import {
   Card,
   Heading,
   Layout,
+  List,
   Page,
   TextContainer,
   TextStyle,
@@ -29,6 +30,43 @@ const Help = () => (
             <p>
               StockTaker turns your mobile phone or tablet into a barcode
               scanning machine and helps you do effortless stock counts.
+            </p>
+            <Heading element="h1">How do you use StockTaker?</Heading>
+            <p>
+              <List type="bullet">
+                <List.Item>
+                  Download StockTaker mobile app from the iOS and Google app
+                  stores.
+                </List.Item>
+                <List.Item>
+                  Install StockTaker on your Shopify store, this will generate a
+                  QR code.
+                </List.Item>
+                <List.Item>
+                  Scan this QR code sign into your StockTaker account.
+                </List.Item>
+                <List.Item>
+                  Select your inventory location for the stock count.
+                </List.Item>
+                <List.Item>
+                  Scan barcodes using the camera on your mobile device.
+                </List.Item>
+                <List.Item>
+                  Once your'e done scanning, decide if you want to Add, Subtract
+                  or Overwrite inventory counts.
+                </List.Item>
+                <List.Item>
+                  Also decide if you want to include all related variants in the
+                  count.
+                </List.Item>
+                <List.Item>
+                  Press OK to update you inventory on Shopify.
+                </List.Item>
+                <List.Item>
+                  After completing, you'll be emailed a CSV report of all the
+                  changes made.
+                </List.Item>
+              </List>
             </p>
             <Heading element="h1">How do I get help?</Heading>
             <p>
